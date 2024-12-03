8d896b5c897c49b5dea0883020f7900cea4607b7
Matthias Liedtke <mliedtke@gmx.de>
2024-12-03 08:19:22 +0100

    make day03 more robust

--- day03.py
+++ day03.py
@@ -6,13 +6,12 @@ print(f"part 1 = {sum(int(match[0]) * int(match[1]) for match in regex_mul.finda
 regex_op = re.compile(r"(mul|do|don't)\((([0-9]+),([0-9]+))?\)")
 enabled = True
 result = 0
-for op, _, a, b in regex_op.findall(input):
-    if op == "do":
+for op, args, a, b in regex_op.findall(input):
+    if op == "do" and args == "":
         enabled = True
-    elif op == "don't":
+    elif op == "don't" and args == "":
         enabled = False
-    else:
-        assert op == "mul"
+    elif op == "mul" and args != "":
         if enabled:
             result += int(a) * int(b)
 print(f"part 2 = {result}")
