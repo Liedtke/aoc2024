a0d0cb96aea769f7b4cd7d1125b624b093d51cff
Matthias Liedtke <mliedtke@gmx.de>
2024-12-05 21:34:09 +0100

    Revert "remove dictionary for positions, doesn't matter vs. linear scan"
    Never mind, it is indeed slightly faster and only a few extra lines.
    
    This reverts commit eac65a03f1da185669a4d5a7894e829b8aaee056.

--- day05.py
+++ day05.py
@@ -2,20 +2,23 @@ inputs = [section.split("\n") for section in open("inputs/day05.txt").read().spl
 rules, page_lists = ([list(map(int, line.split(sep))) for line in section] for section, sep in zip(inputs, ("|", ",")))
 
 def valid_order(pages):
-    return not any(before in pages and after in pages and pages.index(before) > pages.index(after) for before, after in rules)
+    pos = {page: i for i, page in enumerate(pages)}
+    return not any(before in pos and after in pos and pos[before] > pos[after] for before, after in rules)
 
 print(f"part 1 = {sum(pages[len(pages) // 2] if valid_order(pages) else 0 for pages in page_lists)}")
 
 invalid = [pages[:] for pages in page_lists if not valid_order(pages)]
 for pages in invalid:
-    filtered_rules = [rule for rule in rules if rule[0] in pages and rule[1] in pages]
+    pos = {page: i for i, page in enumerate(pages)}
+    filtered_rules = [rule for rule in rules if rule[0] in pos and rule[1] in pos]
     swapped = True
     while swapped:
         swapped = False
         for before, after in filtered_rules:
-            if pages.index(before) > pages.index(after):
-                del pages[pages.index(before)]
-                pages.insert(pages.index(after), before)
+            if pos[before] > pos[after]:
+                del pages[pos[before]]
+                pages.insert(pos[after], before)
+                pos = {page: i for i, page in enumerate(pages)}
                 swapped = True
 
 print(f"part 2 = {sum(pages[len(pages) // 2] for pages in invalid)}")
