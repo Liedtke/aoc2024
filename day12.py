530b22fdf437176fc00ebbfae0339c4a6c74c4fe
Matthias Liedtke <mliedtke@gmx.de>
2024-12-18 00:12:27 +0100

    day 12 replace recursion with stack

--- day12.py
+++ day12.py
@@ -8,33 +8,31 @@ def in_bounds(y, x):
 
 def explore_region(y, x):
     val = input[y][x]
-    region = {"perimeter1": 0, "perimeter2": 0, "area": 0}
-
-    def explore(y, x):
+    perimeter1, perimeter2, area = 0, 0, 0
+    stack = [(y, x)]
+    while stack:
+        y, x = stack.pop()
         assert input[y][x] == val
         if marked[y][x]:
-            return
+            continue
         marked[y][x] = True
-        region["area"] += 1
+        area += 1
 
         for i, (dy, dx) in enumerate(directions):
             ny, nx = y + dy, x + dx
             if in_bounds(ny, nx) and input[ny][nx] == val:
-                explore(ny, nx)
+                stack.append((ny, nx))
             else: # Not the same region, needs fence.
-                region["perimeter1"] += 1
+                perimeter1 += 1
                 # For part 2 check the adjacent values as well. (Check if this fence is a continuation of an existing fence.)
                 dy, dx = directions[i - 1]
                 py, px = y + dy, x + dx
                 npy, npx = ny + dy, nx + dx
                 if not in_bounds(py, px) or input[py][px] != val or (in_bounds(npy, npx) and input[npy][npx] == val):
-                    region["perimeter2"] += 1
+                    perimeter2 += 1
 
-    explore(y, x)
-    price1 = region["area"] * region["perimeter1"]
-    price2 = region["area"] * region["perimeter2"]
-    # print(f"region {val}: area={region['area']} perimeter1={region['perimeter1']} price1={price1} perimeter2={region['perimeter2']} price2={price2}")
-    return price1, price2
+    # print(f"region {val}: area={area} perimeter1={perimeter1} price1={area*perimeter1} perimeter2={perimeter2} price2={area*perimeter2}")
+    return area * perimeter1, area * perimeter2
 
 results = [0, 0]
 for y, line in enumerate(input):
