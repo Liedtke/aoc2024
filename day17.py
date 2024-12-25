ef9aa2abdd3bde82e6198409ef79609fa68a2a91
Matthias Liedtke <mliedtke@gmx.de>
2024-12-25 11:31:26 +0100

    slightly nicer part 1

--- day17.py
+++ day17.py
@@ -1,4 +1,4 @@
-reg, program = open("inputs/day17b.txt").read().split("\n\n")
+reg, program = open("inputs/day17.txt").read().split("\n\n")
 regs = [int(r[12:]) for r in reg.split("\n")]
 program = [int(n) for n in program[9:].split(",")]
 pc = 0
@@ -8,23 +8,23 @@ def combo(op):
     return regs[op - 4] if 4 <= op <= 6 else op
 
 def adv(a):
-    regs[0] = regs[0] // (2 ** combo(a))
+    regs[0] = regs[0] >> combo(a)
 def bxl(a):
-    regs[1] = regs[1] ^ a
+    regs[1] ^= a
 def bst(a):
-    regs[1] = combo(a) % 8
+    regs[1] = combo(a) & 7
 def jnz(a):
     if regs[0] != 0:
         global pc
         pc = a - 2
 def bxc(_):
-    regs[1] = regs[1] ^ regs[2]
+    regs[1] ^= regs[2]
 def out(a):
-    output.append(combo(a) % 8)
+    output.append(combo(a) & 7)
 def bdv(a):
-    regs[1] = regs[0] // (2 ** combo(a))
+    regs[1] = regs[0] >> combo(a)
 def cdv(a):
-    regs[2] = regs[0] // (2 ** combo(a))
+    regs[2] = regs[0] >> combo(a)
 
 ops = [adv, bxl, bst, jnz, bxc, out, bdv, cdv]
 try:
