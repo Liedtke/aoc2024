a45f936b4b56a36d824fb5b545610ac8595989cc
Matthias Liedtke <mliedtke@gmx.de>
2024-12-25 14:30:08 +0100

    day 17 part 2

--- day17.py
+++ day17.py
@@ -27,10 +27,53 @@ def cdv(a):
     regs[2] = regs[0] >> combo(a)
 
 ops = [adv, bxl, bst, jnz, bxc, out, bdv, cdv]
-try:
-    while True:
-        op = program[pc+1]
-        ops[program[pc]](op)
-        pc += 2
-except IndexError:
-    print(",".join(map(str, output)))
+def run():
+    global pc
+    try:
+        while True:
+            op = program[pc+1]
+            ops[program[pc]](op)
+            pc += 2
+    except IndexError:
+        return output
+
+run()
+print(f"part 1 = {','.join(map(str, output))}")
+
+
+def try_it(val, num_matched):
+    global pc
+    global output
+    pc = 0
+    regs[0] = val
+    output = []
+    run()
+    if len(output) <= num_matched:
+        return -1
+    for i, v in enumerate(reversed(output)):
+        if v != program[-i-1]:
+            return -1
+    return len(output)
+
+def part2(expected, inputs, num_matched):
+    if num_matched == len(expected):
+        return inputs
+    bits = 0
+    res = []
+    new_num_matched = 0
+    while not res:
+        for input in inputs:
+            input = input << bits
+            for i in range(1 << bits):
+                val = input + i
+                matched = try_it(val, num_matched)
+                if matched > num_matched:
+                    if matched > new_num_matched:
+                        new_num_matched = matched
+                        res = [val]
+                    elif matched == new_num_matched:
+                        res.append(val)
+        bits += 1
+    return part2(expected, res, new_num_matched)
+
+print(f"part 2 = {min(part2(program, [0], 0))}")
