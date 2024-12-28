0d5bb5b8e3e33fdde577c62e0bff8361bef8c3a4
Matthias Liedtke <mliedtke@gmx.de>
2024-12-28 15:01:17 +0100

    day20: re-use part 2 for part 1

--- day20.py
+++ day20.py
@@ -15,25 +15,18 @@ while not finished:
             y, x = ny, nx
             break
 
-limit = 100
-cheats = 0
-for y, x in path:
-    for dy, dx in ((2, 0), (0, 2), (-2, 0), (0, -2)):
-        ny, nx = y + dy, x + dx
-        if 0 <= ny < len(grid) and 0 <= nx < len(grid[ny]) and isinstance(grid[ny][nx], int) \
-            and grid[ny][nx] >= grid[y][x] + limit + 2:
-            cheats += 1
-print(f"part 1 = {cheats}")
+def find_cheats(length):
+    cheats = 0
+    for y, x in path:
+        for dy in range(-length, length+1):
+            ny = y + dy
+            if 0 <= ny < len(grid):
+                for dx in range(-(length-abs(dy)), length-abs(dy)+1):
+                    nx = x + dx
+                    if 0 <= nx < len(grid[ny]) and isinstance(grid[ny][nx], int) \
+                        and grid[ny][nx] >= grid[y][x] + 100 + abs(dy) + abs(dx):
+                            cheats += 1
+    return cheats
 
-cheats2 = 0
-length = 20
-for y, x in path:
-    for dy in range(-length, length+1):
-        ny = y + dy
-        if 0 <= ny < len(grid):
-            for dx in range(-(length-abs(dy)), length-abs(dy)+1):
-                nx = x + dx
-                if 0 <= nx < len(grid[ny]) and isinstance(grid[ny][nx], int) \
-                    and grid[ny][nx] >= grid[y][x] + limit + abs(dy) + abs(dx):
-                        cheats2 += 1
-print(f"part 2 = {cheats2}")
+print(f"part 1 = {find_cheats(2)}")
+print(f"part 2 = {find_cheats(20)}")
