1d4b567280ea41498e73f39481ece8f9cf8c6372
Matthias Liedtke <mliedtke@gmx.de>
2024-12-28 13:58:02 +0100

    day 19 part 2

--- day19.py
+++ day19.py
@@ -1,18 +1,24 @@
+import heapq
+from collections import defaultdict
 towels, patterns = open("inputs/day19.txt").read().split("\n\n")
 towels, patterns = set(towels.split(", ")), patterns.split("\n")
-res = 0
-max_towel_len = max(map(len, towels))
+patterns_1 = set()
+part_2 = 0
+pattern_counts = {towel: 1 for towel in towels}
+
 for pattern in patterns:
-    tried = set()
-    print(pattern)
+    counts = defaultdict(int)
+    counts[0] = 1
     starts = [0]
     while starts:
-        s = starts.pop()
-        tried.add(s)
+        s = heapq.heappop(starts)
         if s == len(pattern):
-            res += 1
-            break
-        for i in range(1, min(len(pattern) - s, max_towel_len) + 1):
-            if pattern[s:s+i] in towels and s+i not in tried:
-                starts.append(s+i)
-print(f"part 1 = {res}")
+            patterns_1.add(pattern)
+        for i in range(1, len(pattern) - s + 1):
+            if pattern[s:s+i] in towels:
+                if s+i not in counts:
+                    heapq.heappush(starts, s+i)
+                counts[s+i] += counts[s]
+    part_2 += counts[len(pattern)]
+print(f"part 1 = {len(patterns_1)}")
+print(f"part 2 = {part_2}")
